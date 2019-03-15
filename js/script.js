04eb364a72b5fa7315a291f13d882052d95801b8
unknown <tyshinkle@gmail.com>
2019-03-15 16:17:04 -0500

    Completed Greeting

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var salutation="Hello, ";
+var user="friend";
+var greeting=salutation+user+". Are you ready to learn?";
+
+var greetingEl=document.getElementById('greeting');
+greetingEl.textContent=greeting;
+
+//homepage greeting complete
